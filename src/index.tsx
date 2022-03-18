71705bdf780a87b4fdb6eab603fc95570647900c
kosukekashiwa <91003636+kosukekashiwa@users.noreply.github.com>
2022-03-18 17:06:55 +0900

    install MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "../src/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { App } from '../src/App';
+import theme from './views/theme';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
