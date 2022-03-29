78c7e8be9739953b2b19a0293930f6d71b7f68e6
kosukekashiwa <91003636+kosukekashiwa@users.noreply.github.com>
2022-03-29 18:17:48 +0900

    grid layout

--- src/views/natures/OverView.tsx
+++ src/views/natures/OverView.tsx
@@ -3,29 +3,21 @@ import { Box, Grid, Stack } from '@mui/material';
 import { blue } from '@mui/material/colors';
 
 const OverView: React.VFC = () => {
-  const gridItem: React.ReactNode = (
-    <Grid item xs={6} sm={3}>
-      <Box sx={{ border: '1px solid blue' }}>xs=6 sm=3</Box>
-    </Grid>
-  );
   const pallete: number[] = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
   return (
     <Box>
       <Box>Grid Layout (レスポンシブ確認)</Box>
       <Grid container spacing={{ xs: 1, sm: 3 }}>
-        {gridItem}
-        {gridItem}
-        {gridItem}
-        {gridItem}
-        {gridItem}
-        {gridItem}
-        {gridItem}
-        {gridItem}
-        {gridItem}
-        {gridItem}
+        {pallete.map((code) => (
+          <Grid key={code} item xs={6} sm={3} md={1.5}>
+            <Box p={2} sx={{ background: blue[code], color: code < 500 ? '#000000' : '#ffffff' }}>
+              {code}
+            </Box>
+          </Grid>
+        ))}
       </Grid>
-      <Box>OverView (スクロール確認)</Box>
+      <Box mt={2}>OverView (スクロール確認)</Box>
       <Stack>
         {pallete.map((code) => (
           <Box
