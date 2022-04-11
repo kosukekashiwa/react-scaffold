13e98f138c65ffc2aa5e6ed926fa4a059039c10e
kosukekashiwa <91003636+kosukekashiwa@users.noreply.github.com>
2022-04-11 22:22:56 +0900

    msw res.once sample

--- src/views/natures/CheckFetchView.test.tsx
+++ src/views/natures/CheckFetchView.test.tsx
@@ -15,27 +15,27 @@ describe('CheckFetchView tests', () => {
       expect(await result.findByText('name-2')).toBeInTheDocument();
     });
 
-    // describe('res.once sample', () => {
-    //   server.use(
-    //     rest.get(`/api/v1/articles`, (req, res, ctx) => {
-    //       return res.once(
-    //         ctx.status(200),
-    //         ctx.json([
-    //           { id: 0, name: 'name-0' },
-    //           { id: 1, name: 'name-1' },
-    //           { id: 2, name: 'name-2' },
-    //           { id: 3, name: 'name-3' },
-    //           { id: 4, name: 'name-4' },
-    //         ]),
-    //       );
-    //     }),
-    //   );
-    //   it('tmp msw override', async () => {
-    //     const result = render(<CheckFetchView />);
+    describe('res.once sample', () => {
+      server.use(
+        rest.get(`/api/v1/users`, (req, res, ctx) => {
+          return res.once(
+            ctx.status(200),
+            ctx.json([
+              { id: 0, name: 'name-0' },
+              { id: 1, name: 'name-1' },
+              { id: 2, name: 'name-2' },
+              { id: 3, name: 'name-3' },
+              { id: 4, name: 'name-4' },
+            ]),
+          );
+        }),
+      );
+      it('tmp msw override', async () => {
+        const result = render(<CheckFetchView />);
 
-    //     expect(await result.findByText('name-3')).toBeInTheDocument();
-    //     expect(await result.findByText('name-4')).toBeInTheDocument();
-    //   });
-    // });
+        expect(await result.findByText('name-3')).toBeInTheDocument();
+        expect(await result.findByText('name-4')).toBeInTheDocument();
+      });
+    });
   });
 });
