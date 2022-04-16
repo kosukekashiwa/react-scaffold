299e963fadb300d9fac5e073ab3a1c6b76f371c4
kosukekashiwa <91003636+kosukekashiwa@users.noreply.github.com>
2022-04-16 23:03:49 +0900

    Button test

---
+++ src/views/components/actions/ Button.test.tsx
@@ -0,0 +1,49 @@
+import { render, fireEvent } from '../../test-utils';
+import Button from './Button';
+import React from 'react';
+
+describe('Button tests', () => {
+  describe('initisal rendering tests', () => {
+    it('childrenで受け取った文字列を表示', () => {
+      const handler = jest.fn();
+
+      const result = render(
+        <Button variant="contained" color="primary" onClick={handler}>
+          Test
+        </Button>,
+      );
+
+      expect(result.getByText('Test')).toBeTruthy();
+    });
+  });
+
+  describe('action tests', () => {
+    it('Buttonをクリックするとpropsで設定したイベントハンドラが呼ばれる', () => {
+      const handler = jest.fn();
+
+      const result = render(
+        <Button variant="contained" color="primary" onClick={handler}>
+          Test
+        </Button>,
+      );
+      fireEvent.click(result.getByText('Test'));
+
+      expect(handler).toHaveBeenCalledTimes(1);
+      expect(handler).not.toHaveBeenCalledTimes(2);
+    });
+
+    it('Buttonを2回クリックするとpropsで設定したイベントハンドラが2回呼ばれる', () => {
+      const handler = jest.fn();
+
+      const result = render(
+        <Button variant="contained" color="primary" onClick={handler}>
+          Test
+        </Button>,
+      );
+      fireEvent.click(result.getByText('Test'));
+      fireEvent.click(result.getByText('Test'));
+
+      expect(handler).toHaveBeenCalledTimes(2);
+    });
+  });
+});
