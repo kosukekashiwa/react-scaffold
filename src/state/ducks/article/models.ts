b20079a2aeb5200d911ae0b5c3f9be75d904d1e5
kosukekashiwa <91003636+kosukekashiwa@users.noreply.github.com>
2022-04-23 00:05:45 +0900

    jsdoc comment of models

--- src/state/ducks/article/models.ts
+++ src/state/ducks/article/models.ts
@@ -7,30 +7,34 @@ import {
   NormalizedUsers,
 } from '~/state/ducks/user/models';
 
+/** The type of Article. */
 export type Article = {
   id: number;
   title: string;
   author: User;
 };
 
-// Articleの正規化されたときの型はauthorがUserのid型となる
+/** The type of normalized Article. */
 export type NormalizedArticle = Omit<Article, 'author'> & {
   author: User['id'];
 };
 
+/** The normalizr schema key of Article. */
 export const articleNormalizrSchemaKey = 'articles';
 
-// authorにはUserのスキーマkeyを指定
+/** The normalizr schema of Article. */
 export const articleNormalizrSchema = new schema.Entity<Article>(
   articleNormalizrSchemaKey,
   { author: userNormalizrSchema },
   { idAttribute: 'id' },
 );
 
+/** The type of normalized Articles. */
 export type NormalizedArticles = {
   [id: number]: NormalizedArticle;
 };
 
+/** Normalized Articles. */
 export const normalizeArticles = (articles: Article[]) =>
   normalize<
     Article,
@@ -38,5 +42,6 @@ export const normalizeArticles = (articles: Article[]) =>
     Article['id'][]
   >(articles, [articleNormalizrSchema]);
 
+/** Denormalized Articles. */
 export const denormalizeArticles = (articles: ReturnType<typeof normalizeArticles>): Article[] =>
   denormalize(articles.result, [articleNormalizrSchema], articles.entities);
--- src/state/ducks/user/models.ts
+++ src/state/ducks/user/models.ts
@@ -1,37 +1,35 @@
 import { denormalize, normalize, schema } from 'normalizr';
 
+/** The type of User. */
 export type User = {
   id: number;
   name: string;
 };
 
-// 正規化された状態の型
-// Userは別Tableの情報を持たないので変わらないが、可読性を上げたいので定義する
+/** The type of normalized User. */
 export type NormalizedUser = User;
 
-// スキーマのkeyはslices.tsでも使用するのでexportしておく
+/** The normalizr schema key of User. */
 export const userNormalizrSchemaKey = 'users';
 
-// スキーマの定義
-// デフォルトでidを主キーとしてくれるが、idAttributeはid以外を主キーとしたい場合に使用する
-// 今回、idを指定しているので別になくてもよい
+/** The normalizr schema of User. */
 export const userNormalizrSchema = new schema.Entity<User>(
   userNormalizrSchemaKey,
   {},
   { idAttribute: 'id' },
 );
 
-// 正規化されたUserの一覧
+/** The type of normalized Users. */
 export type NormalizedUsers = {
   [id: number]: NormalizedUser;
 };
 
-// User情報を正規化する関数
+/** Normalized Users. */
 export const normalizeUsers = (users: User[]) =>
   normalize<User, { [userNormalizrSchemaKey]: NormalizedUsers }, User['id'][]>(users, [
     userNormalizrSchema,
   ]);
 
-// 正規化されたUser情報を元に戻す関数
+/** Denormalized Users. */
 export const denormalizeUsers = (users: ReturnType<typeof normalizeUsers>): User[] =>
   denormalize(users.result, [userNormalizrSchema], users.entities);
