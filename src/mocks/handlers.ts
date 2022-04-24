dc993f575d91ab4c3fd7505929468fc7c62a6ece
kosukekashiwa <91003636+kosukekashiwa@users.noreply.github.com>
2022-04-24 17:16:32 +0900

    format

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 
-import articles from './apis/articles';
-import users from './apis/users';
+import articles from '~/mocks/apis/articles';
+import users from '~/mocks/apis/users';
 
 export const handlers = [
   rest.get(`/api/v1/users`, users.get),
