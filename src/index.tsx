b59f74042ab7b8efe7455f2aff3517ba4a44d694
kosukekashiwa <91003636+kosukekashiwa@users.noreply.github.com>
2022-04-24 19:03:02 +0900

    msw setupWorker

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,14 @@ import { Provider } from 'react-redux';
 
 import { createRoot } from 'react-dom/client';
 
+import { worker } from '~/mocks/worker';
 import { store } from '~/state/store';
 import App from '~/views/App';
+
+// memo: json-serverとmswを切り替えれる様にするか迷い中(mswのみにしようかな?)
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
